8c97df978cfd27868bf6f920fb519a2c92f7d0c7
José Tomás Lavín León <jlavinleon@alumni.unav.es>
2026-07-04 10:40:02 +0200

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):
+            return False
+        # we can count number of appearances
+        # we can sort and compare but that is n*log(n) solution
+        # looking for m+n
+        counterS = {}
+        counterT = {}
+        for i in range(len(s)):
+            counterS[s[i]] = 1+ counterS.get(s[i], 0)
+            counterT[t[i]] = 1+ counterT.get(t[i], 0)
+        
+        return counterS == counterT
+
